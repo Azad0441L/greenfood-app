e297ac6555fa85376d279474e31e53aff4cf82c0
Azad0441L <pcazadgulamhussain2468@gmail.com>
2026-06-10 16:04:40 +0100

    tird commit

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.get("/status", (req, res) => {
 });
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(__dirname, "public/index.html"));
 });
 app.listen(3000, () => {
   console.log("Listen on http://localhost:3000");
