8f67311b00ccd2a083230d9c2eb953715a8215d7
Azad0441L <pcazadgulamhussain2468@gmail.com>
2026-06-10 15:50:20 +0100

    second commit

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 app.use(express.json());
 
 const state = [
   { id: 1, nom: "Azad", prenom: "Gulam hussain" },
 ];
+
 app.get("/status", (req, res) => {
   res.json(state);
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+app.listen(3000, () => {
+  console.log("Listen on http://localhost:3000");
 });
